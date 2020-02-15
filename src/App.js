5ae46f9138b7ec8d36686cf9dec03c321f4cc98d
Abdülkadir Karabaş <abdulkadir.karabas@akinon.com>
2020-02-15 16:05:48 +0300

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,12 +2,19 @@ import React from 'react';
 import './App.css';
 import { Provider } from "react-redux";
 import AppStore from './redux/store/appStore';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from "./pages/home";
+import NotFound from "./pages/notFound";
 
 function App() {
   return (
     <Provider store={AppStore}>
-      <Home />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   );
 }
